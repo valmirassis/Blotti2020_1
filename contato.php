4ec3eaa768c48d5720aa9b188dd7f36f66021760
Assis <assis@rediscover.com.br>
2020-03-05 23:31:47 -0300

    redes sociais

--- contato.php
+++ contato.php
@@ -63,7 +63,7 @@ include ('../admin20/conecta.php');
                           <a class="nav-link anima" href="http://conteudo.blotti.com.br/catalogo" target="_blank"><i class="fas fa-book"></i> Catálogo</a>
                       </li>  
                       <li class="nav-item">
-                          <a class="nav-link anima" href="trabalhe-conosco.php" target="_blank"><i class="fas fa-cubes"></i> Trabalhe conosco</a>
+                          <a class="nav-link anima" href="trabalhe-conosco.php"><i class="fas fa-cubes"></i> Trabalhe conosco</a>
                       </li>         
                         <li class="nav-item">
                                 <a class="nav-link anima" href="contato.php"><i class="fas fa-envelope"></i> Contato</a>
@@ -214,71 +214,8 @@ include ('../admin20/conecta.php');
                   </div>
                      
         </section>
-<section class="footer">
-<div class="container">
-  <div class="row">
-    <div class="col-sm text-center">
-     <h1> B.lotti - Indústria da Movimentação</h1>
-<p>Todos os nossos produtos são resultados de inovação e ótimas ideias, <br>e pela busca incessantes de excelência para movimentação e amarração de cargas seguras.</p>
-<br>
-<a href="#" class="btn btn-success btn-blotti">Solicitar Orçamento</a>
-    </div>
-  </div>
-  <br>
-  <div class="row text-center" style="margin-top: 30px;">
-    <div class="col-sm">
-      <i class="fas fa-phone-square icon-footer-big"></i><br>
-      <b>Contatos</b><br> 
-      47 3307-3800<br>
-      blotti@blotti.com.br<br>
-    </div>
-    <div class="col-sm">
-      <i class="fas fa-map-marker-alt icon-footer-big"></i><br>
-      <b>Localização</b> <br>
-      KM 60 BR 280, 15765, Imigrantes, Guaramirim/SC.
-     
-    </div>
-    <div class="col-sm">
-      <i class="fas fa-clock icon-footer-big"></i> <br>
-      
-      <b>Horário de Atendimento</b><br>
-      De segunda a sexta das 08:00 às 12:00  e das 13:15 às 18:00
-      </div>
 
-<!-- 
-    <div class="col-sm">
-      <h3>CENTRAL DE ATENDIMENTO</h3> <br>
-      <div class="status">
-        <a href="https://www.linkedin.com/company/blottimovimentacao" target="_blank"><i class="fab fa-linkedin"></i>&nbsp;<b>/blottimovimentacao</b> </a></div><br>
-  <div class="status">
-        <a href="https://www.instagram.com/b.lottimovimentacao/" target="_blank"><i class="fab fa-instagram"></i>&nbsp;<b>/b.lottimovimentacao</b> </a> </div>
-<br>
-          <div class="status">
-       <a href="https://www.facebook.com/blottimovimentacao/" target="_blank"><i class="fab fa-facebook-square"></i>&nbsp;<b>/blottimovimentacao</b> </a> </div>
-       <br>
-              <div class="status">
-                <a href="#" target="_blank"><i class="fas fa-phone-square"></i>&nbsp;<b>47 3307-3800</b> </a> </div><br>
-                <div class="status">
-                  <a href="#" target="_blank"><i class="fas fa-envelope"></i>&nbsp;<b>blotti@blotti.com.br</b> </a> </div>
-            </div>
-              <div class="col-sm">
-                <h3>FUTURAS FEIRAS</h3> <br>
-                <img src="img/feira1.jpg" width="200px">
-              </div> -->
-    </div>
-   
-  </div>
-</div>
-</section>
-<section class="footer2">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm">
-        © 2020 | POLÍTICA DA PRIVACIDADE | COMPLIENCE | CERTIFICADOS | INTRANET | EXTRANET
-      </div>
-    </div>
-  </div>
-</section>
+        <?php include ('footer.php');?>
 
 <script>
        $(document).ready(function(){
---
+++ footer.php
@@ -0,0 +1,76 @@
+<section class="footer">
+<div class="container">
+  <div class="row">
+    <div class="col-sm text-center">
+     <h1> B.lotti - Indústria da Movimentação</h1>
+<p>Todos os nossos produtos são resultados de inovação e ótimas ideias, <br>e pela busca incessantes de excelência para movimentação e amarração de cargas seguras.</p>
+<br>
+<a href="#" class="btn btn-success btn-blotti">Solicitar Orçamento</a>
+    </div>
+  </div>
+  <br>
+  <div class="row text-center" style="margin-top: 30px;">
+    <div class="col-sm">
+      <i class="fas fa-phone-square icon-footer-big"></i><br>
+      <b>Contatos</b><br> 
+      47 3307-3800<br>
+      blotti@blotti.com.br<br>
+    </div>
+    <div class="col-sm">
+      <i class="fas fa-map-marker-alt icon-footer-big"></i><br>
+      <b>Localização</b> <br>
+      KM 60 BR 280, 15765, Imigrantes, Guaramirim/SC.
+     
+    </div>
+    <div class="col-sm">
+      <i class="fas fa-clock icon-footer-big"></i> <br>
+      
+      <b>Horário de Atendimento</b><br>
+      De segunda a sexta das 08:00 às 12:00  e das 13:15 às 18:00
+      </div>
+
+    </div>
+   
+  </div>
+</div>
+</section>
+<section class="redes-sociais">
+<div class="container">
+  <div class="row">
+  <div class="col-md-3 col-sm">
+             
+                <a href="https://www.linkedin.com/company/blottimovimentacao" target="_blank"><i class="fab fa-linkedin"></i></a>
+                
+  
+</div>
+<div class="col-md-3 col-sm">
+            
+                <a href="https://www.instagram.com/b.lottimovimentacao/" target="_blank"><i class="fab fa-instagram"></i><br> </a> 
+                  
+  
+</div>
+
+<div class="col-md-3 col-sm">
+                
+                <a href="https://www.facebook.com/blottimovimentacao/" target="_blank"><i class="fab fa-facebook-square"></i> </a> 
+  
+</div>
+<div class="col-md-3 col-sm">
+           
+                <a href="https://www.youtube.com/channel/UCuCACO9WmAsAZNfxfdiIOXQ" target="_blank"><i class="fab fa-youtube"></i> </a> 
+                  
+</div>
+ 
+
+</div>
+</div>
+</section>
+<section class="footer2">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        © 2020 | POLÍTICA DA PRIVACIDADE | COMPLIENCE | CERTIFICADOS | INTRANET | EXTRANET
+      </div>
+    </div>
+  </div>
+</section>
